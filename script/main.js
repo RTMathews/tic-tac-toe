44b6368faecb24f5e68908436bdc83d05a3a0c9a
Ryan <mathewsthomasryan@gmail.com>
2024-03-23 09:53:51 -0500

    players

--- script/main.js
+++ script/main.js
@@ -1,13 +1,34 @@
-(function() {
-    const gameBoard = {
-        board: [],
-        init: function() {
-            this.cacheDom();
-        },
-        cacheDom: function() {
-            this.$el = $("#game-board");
-        }
-    };
+const Gameboard = (() => {
+    let gameBoard = ['','','','','','','','','']
 
-    gameBoard.init();
-})()
+    const render = () => {
+        let boardHTML = '';
+        gameBoard.forEach((tile, index) => {
+            boardHTML += `<div class="tile" id=tile-${index}>${tile}</div>`
+        })
+    }
+    document.querySelector('#game-board').boardHTML
+
+    return {
+        render,
+    }
+})();
+
+const createPlayer = (name, mark) => {
+    return {
+        name,
+        mark
+    }
+}
+
+const Game = (() => {
+    let players = [];
+    let currentPlayer = 0;
+    let gameOver = false;
+
+    const start = () => {
+        players = [
+            createPlayer()
+        ]
+    }
+})();
