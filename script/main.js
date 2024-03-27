a1bf21eb3bfd44c80d93f1790a34c393b6188aa6
Ryan <mathewsthomasryan@gmail.com>
2024-03-27 12:20:11 -0500

    restart

--- script/main.js
+++ script/main.js
@@ -1,9 +1,47 @@
-const Game = (() => {
-    const tiles = document.querySelectorAll('.tile');
-    const clickHandler = () => {
-        
-    }
-    tiles.forEach((tile) => {
-        tile.addEventListener('click', clickHandler, {once : true});
-    });
+const Gameboard = (() => {
+
 })();
+
+const createPlayer = (name, marker) => {
+    return {
+        name,
+        marker,
+    }
+}
+
+const Game = (() => {
+    let currentPlayer = 0;
+    let gameOver = false;
+    const start = () => {
+        let players = [
+            createPlayer(document.querySelector('#player1').value, 'X'),
+            createPlayer(document.querySelector('#player2').value, 'O')
+    ]
+    const gameboard = Array.from(document.querySelectorAll('.tile'))
+    const handleBoard = (e) => {
+        e.target.textContent = players[currentPlayer].marker
+        currentPlayer = currentPlayer === 0 ? 1 : 0;
+    }
+    gameboard.forEach((tile) => {
+        tile.addEventListener('click', handleBoard, {once : true})
+    })
+    }
+
+    const restart = () => {
+        window.location.reload();
+    }
+    return {
+        start,
+        restart,
+    }
+})();
+
+const restartButton = document.querySelector('#restart-btn');
+restartButton.addEventListener('click', () => {
+    Game.restart();
+})
+
+const startButton = document.querySelector('#start-btn');
+startButton.addEventListener('click', () => {
+    Game.start();
+});
